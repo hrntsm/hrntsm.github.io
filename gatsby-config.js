141740403d066cbc0e19d2f4e30a87377fe39b8e
hrntsm <contact@hrntsm.com>
2020-09-19 23:41:53 +0900

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,12 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-178476251-1",
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
